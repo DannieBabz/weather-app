7270d0d3b7c757d25d973b09e3b247b0744c2c90
Babatunde Daniel <[similoluwababatunde@gmail.com]>
2024-02-05 02:19:18 +0100

    Updated Fetch logic

--- src/Widgets.jsx
+++ src/Widgets.jsx
@@ -1,13 +1,24 @@
-import { useFetch } from "react";
+import { useFetch } from "./useFetch";
 
 const Widgets = () => {
-    const {data, isPending} = useFetch()
+
+  const [data, isPending, city] = useFetch()
+
 
     return ( 
         <>
             <div className="grid gap-20 grid-cols-4 w-screen">
                 {isPending && <div className="widget">Loading...</div> }
-                {data && <div className="widget"></div>}
+                {data && 
+                  city.map(() => {
+                    <div className="widget">
+                    <h2>{data.name}</h2>
+                    <p>Temperature: {Math.round(data.main.temp - 273.15)}&deg;C</p>
+                    <p>Weather: {data.weather[0].description}</p>
+                    <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" />
+                  </div>
+                  })
+                  }
             </div>
         </>
      );
--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
-
-let locations = ["lagos", "abuja", "london", "newyork", "sydney", "accra", "hongkong", "nairobi"];
+import { useState, useEffect } from "react";
 
 const useFetch = () => {
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  locations.map((city) => {
-    const API_KEY = "49e70472fb0fb3c11706ed617cb17a88";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-
-    console.log(url);
-
-    fetch(url)
+      // const {data, isPending} = useFetch()
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [city, setCity] = useState([])
+    // const API_KEY = "49e70472fb0fb3c11706ed617cb17a88";
+    
+    useEffect(() => {
+      const locations = ["lagos", "abuja", "london", "texas", "sydney", "accra", "hongkong", "nairobi"];
+      locations.map(() => {
+      // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+      fetch("./src/Weather.json")
       .then((response) => response.json())
       .then((coords) => {
-        setData(coords);
-        setIsPending(false);
-        console.log(coords);
-        console.log(Math.round(coords.main.temp - 273.15));
-      });
-  });
+        setCity(locations)
+      setData(coords);
+      setIsPending(false);
+      console.log(coords);
+      // return <h1>${coords.main.temp - 273.15}</h1>
+      // <p>${coords.weather[0].main}</p>
+    });
+    
 
-  return { data, isPending };
+    });
+}, [])
+    
+  return { data, isPending, city };
 };
 
 export default useFetch;
