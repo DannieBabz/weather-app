d2a3192a1dc558c77d4d73f4bf02d3a1fdd88be8
Babatunde Daniel <daniel.babatunde@whytelabeltech.com>
2026-03-11 12:26:04 +0100

    changed current weather text color

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -20,10 +20,10 @@ const CurrentWeather = ({lat, lon, cardinal1, data, cardinal2}) => {
 
         {temp == null ? 
             (<div className="text-5xl"><small>Loading...</small></div>
-            ) : (<div className="text-5xl">
+            ) : (<div className="mx-auto text-5xl">
                 <small>Today</small><br />
                 <small>{lat}&deg;{cardinal1} {lon}&deg;{cardinal2}({data?.name})</small><br />
-                <h2 className="text-5xl text-bold text-white">{Math.round(temp -273.15)}&deg;C</h2>
+                <h2 className="text-5xl text-bold text-slate-500">{Math.round(temp -273.15)}&deg;C</h2>
             </div>)}
 
     
